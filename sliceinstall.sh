34610c777c8d7f93f3c42e203fa3a69c36b3c327
Andres <andres@sliceup.co>
2020-07-02 14:59:28 -0300

    Add taskmaster support

--- sliceinstall.sh
+++ sliceinstall.sh
@@ -95,7 +95,7 @@ export SSHPASS="$sspass1"
             insed="echo $sspass1 | sudo -S sed -i 's/{MASTER_IP}/$masterip/g' /opt/sliceup/executables/flink-1.10.0/conf/flink-conf.yaml"
 	        sshpass -ev ssh -t -o "StrictHostKeyChecking=no"  $address "$insed"  
 
-            sshpass -ev ssh -o "StrictHostKeyChecking=no" $address /opt/sliceup/scripts/workerstart.sh
+            sshpass -ev ssh -o "StrictHostKeyChecking=no" $address "python3 /opt/sliceup/executables/task-exec-monitor.py $address"
 
          done
 
@@ -212,11 +212,13 @@ export SSHPASS="$sspass1"
     sudo apt-get install python-dev -y
     sudo apt-get install python3-dev -y
     sudo apt-get install build-essential -y
-    sudo apt install python-pip -y
-    pip install psycopg2
-    pip install PrettyTable
+    sudo apt install python3-pip
+    python3 -m pip install psycopg2
+    python3 -m pip install requests
+    python3 -m pip install PrettyTable
+    python3 -m pip install selenium
     sudo apt-get install build-essential autoconf libtool pkg-config python-opengl python-pil python-pyrex python-pyside.qtopengl idle-python2.7 qt4-dev-tools qt4-designer libqtgui4 libqtcore4 libqt4-xml libqt4-test libqt4-script libqt4-network libqt4-dbus python-qt4 python-qt4-gl libgle3 python-dev -y
-    pip install kafka-python
+    python3 -m pip install kafka-python
 
     echo -e "\e[96m Create Zookeeper Config  \e[39m"
 
@@ -271,6 +273,8 @@ export SSHPASS="$sspass1"
     sudo mv sliceprov.yaml /etc/grafana/provisioning/dashboards
     sudo mv slicedatasource.yaml /etc/grafana/provisioning/datasources
 
+    
+
 ####Begin Master Start#####
 echo -e "\e[96m Installation is complete. Begin Master Service start?  \e[39m"
 read ready
@@ -285,6 +289,17 @@ read ready
     sudo /bin/systemctl daemon-reload
     sudo /bin/systemctl enable grafana-server
     sudo /bin/systemctl start grafana-server
+
+    sudo apt-get install -y jq
+    echo -e "\e[96m Restarting Grafana \e[39m"
+    sudo /bin/systemctl stop grafana-server
+    sleep 5
+    sudo /bin/systemctl start grafana-server
+    sleep 5
+    echo -e "\e[96m Changing Home Dashboard \e[39m"
+    id=$(curl -X GET -H "Content-Type: application/json" http://admin:admin@127.0.0.1:3000/api/dashboards/uid/kC8AXaZMz | jq .dashboard.id)
+    echo -e "\e[96m Dashboard ID is $id \e[39m"
+    curl -X PUT -H "Content-Type: application/json" -d '{"theme": "", "homeDashboardId": '$id', "timezone": ""}' http://admin:admin@127.0.0.1:3000/api/org/preferences
 
 
 echo -e "\e[96m Start Kafka  \e[39m"
